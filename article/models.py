0cdc2a778268a836559289d4e8b4112b1a0726b9
Yerin Lee <57986882+meekukin@users.noreply.github.com>
2020-03-12 16:37:55 +0900

    Update models.py

--- article/models.py
+++ article/models.py
@@ -1,45 +1,45 @@
 from django.db import models
 
-class Articles(models.Model):
-    title = models.CharField(max_length = 100)
-    image_url = models.URLField(max_length = 2000)
-    created_date = models.DateTimeField(blank=True)
-    caption_date = models.CharField(max_length = 50)
-    caption = models.CharField(max_length = 300)
-    article_detail = models.OneToOneField('ArticleDetails', on_delete=models.CASCADE)
+class Article(models.Model):
+    title          = models.CharField(max_length = 100)
+    image_url      = models.URLField(max_length = 2000)
+    created_date   = models.DateTimeField(blank=True)
+    caption_date   = models.CharField(max_length = 50)
+    caption        = models.CharField(max_length = 300)
+    article_detail = models.OneToOneField('ArticleDetail', on_delete=models.CASCADE)
 
-    category = models.ManyToManyField('Categories', through ='CategoryTagArticles')
-    tag = models.ManyToManyField('Tags', through ='CategoryTagArticles')
+    category       = models.ManyToManyField('Category', through ='CategoryTagArticle')
+    tag            = models.ManyToManyField('Tag', through ='CategoryTagArticle')
 
     class Meta:
         db_table = 'articles'
 
-class ArticleDetails(models.Model):
-    title = models.CharField(max_length = 100)
+class ArticleDetail(models.Model):
+    title        = models.CharField(max_length = 100)
     caption_date = models.CharField(max_length = 50)
-    caption = models.CharField(max_length = 200, null=True)
-    description = models.TextField()
+    caption      = models.CharField(max_length = 200, null=True)
+    description  = models.TextField()
 
     class Meta:
-        db_table = 'article_detail'
+        db_table = 'article_details'
 
-class Categories(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length = 50)
-    tag = models.ManyToManyField('Tags', through ='CategoryTagArticles')
+    tag  = models.ManyToManyField('Tag', through ='CategoryTagArticle')
 
     class Meta:
         db_table = 'categories'
 
-class Tags(models.Model):
+class Tag(models.Model):
     name = models.CharField(max_length = 50)
 
     class Meta:
         db_table = 'tags'
 
-class CategoryTagArticles(models.Model):
-    article = models.ForeignKey('Articles', on_delete= models.SET_NULL, null=True)
-    category = models.ForeignKey('Categories', on_delete= models.SET_NULL, null=True)
-    tag = models.ForeignKey('Tags', on_delete= models.SET_NULL, null=True)
+class CategoryTagArticle(models.Model):
+    article  = models.ForeignKey('Article', on_delete= models.SET_NULL, null=True)
+    category = models.ForeignKey('Categorie', on_delete= models.SET_NULL, null=True)
+    tag      = models.ForeignKey('Tag', on_delete= models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'category_tag_articles'
@@ -48,7 +48,7 @@ class Video(models.Model):
     title            = models.CharField(max_length=100)
     date             = models.CharField(max_length=50)
     background_image = models.URLField(max_length=2500)
-    category         = models.ForeignKey('Categories', on_delete=models.SET_NULL, null= True)
+    category         = models.ForeignKey('Category', on_delete=models.SET_NULL, null= True)
     video_url        = models.URLField(max_length=2500)
     content          = models.CharField(max_length = 5000, null=True)
 
