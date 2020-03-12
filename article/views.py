9d0b9e63c82c3de9f01fbf7c1d44dd1a3d56c75e
Yerin Lee <57986882+meekukin@users.noreply.github.com>
2020-03-12 16:41:00 +0900

    Update views.py

--- article/views.py
+++ article/views.py
@@ -1,12 +1,12 @@
-from django.views import View
-from django.http  import JsonResponse
+from django.views     import View
+from django.http      import JsonResponse
 
-from .models      import (
-    Articles,
-    ArticleDetails,
-    Categories,
-    Tags,
-    CategoryTagArticles,
+from .models          import (
+    Article,
+    ArticleDetail,
+    Category,
+    Tag,
+    CategoryTagArticle,
     Video
 )
 
@@ -14,12 +14,12 @@ from django.db.models import Q
 
 class CategoryView(View):
     def get(self, request):
-        categories = list(Categories.objects.values())
+        categories = list(Category.objects.values())
         return JsonResponse({'Category':categories}, status = 200)
 
 class TagView(View):
     def get(self,request):
-        tags = list(Tags.objects.values())
+        tags = list(Tag.objects.values())
         return JsonResponse({'Tag': tags}, status = 200)
 
 class ArticleView(View):
@@ -35,28 +35,29 @@ class ArticleView(View):
     def get(self, request, category_id):
         try:
             offset = int(request.GET.get('offset', 0))
-            limit = int(request.GET.get('limit', 12))
+            limit  = int(request.GET.get('limit', 12))
 
             if 'tag_id' in request.GET:
-                tag_id = int(request.GET.get('tag_id'))    
-                articles = CategoryTagArticles.objects.select_related('article','category','tag').filter(category_id = category_id, tag_id = tag_id).order_by('id')[offset:limit]
-                data = self._article_data(articles)
+                tag_id   = int(request.GET.get('tag_id'))    
+                articles = CategoryTagArticle.objects.select_related('article','category','tag').filter(category_id = category_id, tag_id = tag_id).order_by('id')[offset:limit]
+                data     = self._article_data(articles)
                 return JsonResponse({'data':data}, status=200)
 
-            articles = CategoryTagArticles.objects.select_related('article','category').filter(category_id = category_id).order_by('id')[offset*limit:(offset+1)*limit]
-            data = self._article_data(articles)
+            articles = CategoryTagArticle.objects.select_related('article','category').filter(category_id = category_id).order_by('id')[offset*limit:(offset+1)*limit]
+            data     = self._article_data(articles)
 
             return JsonResponse({'data':data}, status=200)
+        
         except ValueError:
             return JsonResponse({'message':'INVALID_VALUE'}, status = 400)
 
 class SearchView(View):
     def get(self, request):
         if 'keyword' in request.GET:
-            keyword = request.GET.get('keyword')
-            articles = Articles.objects.filter(Q(title__icontains=keyword)|Q(caption__icontains=keyword))
+            keyword  = request.GET.get('keyword')
+            articles = Article.objects.filter(Q(title__icontains=keyword)|Q(caption__icontains=keyword))
             article_list = [{
-                'title' : article.title,
+                'title'     : article.title,
                 'image_url' : article.image_url,
                 'detail_id' : article.article_detail_id
             } for article in list(articles.all())]
@@ -69,40 +70,42 @@ class DetailView(View):
 
     def get(self, request, detail_id):
         try:
-            limit = request.GET.get('limit', 120)
+            limit       = request.GET.get('limit', 120)
             category_id = request.GET.get('category_id', 1)
 
-            detail = ArticleDetails.objects.get(id=detail_id)
+            detail      = ArticleDetail.objects.get(id=detail_id)
             detail_info = [{
-                'title' : detail.title,
+                'title'        : detail.title,
                 'caption_date' : detail.caption_date,
-                'description' : detail.description
+                'description'  : detail.description
             }]
             
-            articles = CategoryTagArticles.objects.select_related('article').filter(category_id = category_id).order_by('id')[0:limit]
+            articles = CategoryTagArticle.objects.select_related('article').filter(category_id = category_id).order_by('id')[0:limit]
             article_list = [{
-                'title' : props.article.title,
+                'title'        : props.article.title,
                 'caption_date' : props.article.caption_date,
-                'image_url' : props.article.image_url,
-                'caption' : props.article.caption,
-                'detail_id' : props.article.article_detail_id
+                'image_url'    : props.article.image_url,
+                'caption'      : props.article.caption,
+                'detail_id'    : props.article.article_detail_id
             } for props in articles]
             return JsonResponse({'detail':detail_info, 'article_list':article_list}, status=200)
-        except ArticleDetails.DoesNotExist:
+        
+        except ArticleDetail.DoesNotExist:
             return JsonResponse({'message':'INVALID_ID'}, status = 404)
 
 class MainView(View):
     MAIN_TAG = 12
     def get(self, request):
         try:
-            limit = int(request.GET.get('limit', 15))
-            main_articles = CategoryTagArticles.objects.select_related('article','tag').filter(tag_id = self.MAIN_TAG).order_by('id')[0:limit]
-            article_list = [{
-                'title' : props.article.title,
+            limit         = int(request.GET.get('limit', 15))
+            main_articles = CategoryTagArticle.objects.select_related('article','tag').filter(tag_id = self.MAIN_TAG).order_by('id')[0:limit]
+            article_list  = [{
+                'title'     : props.article.title,
                 'image_url' : props.article.image_url,
                 'detail_id' : props.article.article_detail_id
             } for props in main_articles]
             return JsonResponse({'data':article_list}, status=200)
+        
         except ValueError:
             return JsonResponse({'message':'INVALID_VALUE'}, status = 400)
 
@@ -131,5 +134,6 @@ class VideoDetailView(View):
                 'content'          : video.content,
                 } 
             return JsonResponse(data, safe = False, status = 200)
+        
         except Video.DoesNotExist:
             return JsonResponse({'message':'ARTICLE_NOT_FOUND'}, status =404)
